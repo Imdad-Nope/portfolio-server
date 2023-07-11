82d2470b2571479af16c34882187d399fed2672b
Imdad-Nope/Alauddin <mdalauddinovi62@gmail.com>
2023-07-11 23:07:33 +0600

    trying to solve vercel link issue

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
 var cors = require('cors')
-require('dotenv').config()
+const dotenv = require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 5000
 
-app.use(cors())
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n5vc4.mongodb.net/?retryWrites=true&w=majority`; 
 
